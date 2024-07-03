318bdf61a6c0ed4baad35081375b9078d600f7fc
Russell Toll <rtoll@redhawk-tech.com>
2024-07-02 22:03:52 -0400

    First not being able to de selected today, added a new table to displays entrys for the dats selected

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "902901968"
+    serviceWorkerVersion: "3283625426"
   }
 });
